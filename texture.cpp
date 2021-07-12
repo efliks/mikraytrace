5642899bb31f5b2052fa5b659c7857255e0dadb2
Mikolaj Feliks <mikolaj.feliks@gmail.com>
2021-07-12 21:18:49 +0200

    Add texture flyweight

--- texture.cpp
+++ texture.cpp
@@ -10,6 +10,94 @@ static const double kRealToByte = 255;
 static const double kByteToReal = 1 / kRealToByte;
 
 
+TextureSharedState::TextureSharedState(const std::string& texture_filename) {
+    png::image<png::rgb_pixel> image(texture_filename.c_str());
+
+    texture_width_ = image.get_width();
+    texture_heigth_ = image.get_height();
+
+    texture_data_.reserve(texture_width_ * texture_heigth_);
+
+    for (unsigned int i = 0; i < texture_heigth_; i++) {
+        png::rgb_pixel *in = &image[i][0];
+        for (unsigned j = 0; j < texture_heigth_; j++, in++) {
+            MyPixel pixel{in->red, in->green, in->blue};
+            texture_data_.push_back(pixel);
+        }
+    }
+}
+
+
+MyPixel TextureSharedState::pick_pickel(double frac_x, double frac_y, double scale_coeff) const {
+    unsigned int u = (static_cast<unsigned int>(
+                          frac_x * texture_width_ * scale_coeff)) % texture_width_;
+
+    unsigned int v = (static_cast<unsigned int>(
+                          frac_y * texture_heigth_ * scale_coeff)) % texture_heigth_;
+
+    return texture_data_[u + v * texture_width_];
+}
+
+
+MyTexture::MyTexture(const std::string& texture_filename,
+                     double reflection_coeff,
+                     double scale_coeff) :
+    reflection_coeff_(reflection_coeff),
+    scale_coeff_(scale_coeff),
+    is_owner_(true) {
+
+    shared_state_ = new TextureSharedState(texture_filename);
+}
+
+
+MyTexture::MyTexture(MyTexture* other,
+                     double reflection_coeff,
+                     double scale_coeff) :
+    reflection_coeff_(reflection_coeff),
+    scale_coeff_(scale_coeff),
+    is_owner_(false) {
+
+    shared_state_ = other->get_shared_state();
+}
+
+
+MyTexture::~MyTexture() {
+    if (is_owner_) {
+        delete shared_state_;
+    }
+}
+
+
+MyPixel MyTexture::pick_pickel(double frac_x, double frac_y) const {
+    return shared_state_->pick_pickel(frac_x, frac_y, scale_coeff_);
+}
+
+
+MyTexture* TextureFactory::get_texture(const std::string& texture_filename,
+                                       double reflection_coeff,
+                                       double scale_coeff) {
+    if (texture_map_.find(texture_filename) == texture_map_.end()) {
+        // Create new texture data
+        MyTexture new_texture(texture_filename, reflection_coeff, scale_coeff);
+        textures_.push_back(new_texture);
+        MyTexture* texture_ptr = &textures_.back();
+        texture_map_.insert(std::pair<std::string, MyTexture*>(texture_filename, texture_ptr));
+
+        return texture_ptr;
+    }
+
+    // Reuse existing texture data
+    MyTexture* owner_texture_ptr = texture_map_.at(texture_filename);
+    MyTexture new_texture(owner_texture_ptr, reflection_coeff, scale_coeff);
+
+    textures_.push_back(new_texture);
+    MyTexture* texture_ptr = &textures_.back();
+
+    return texture_ptr;
+}
+
+
+
 Texture::Texture(const std::string& texture_filename) :
   texture_filename_(texture_filename) {
 
--- texture.h
+++ texture.h
@@ -4,10 +4,67 @@
 #include <list>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <memory>
 #include "pixel.h"
 
 
 namespace mrtp {
+
+struct MyPixel {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+};
+
+
+class TextureSharedState {
+public:
+    TextureSharedState(const std::string&);
+    TextureSharedState() = delete;
+    ~TextureSharedState() = default;
+
+    MyPixel pick_pickel(double, double, double) const;
+
+private:
+    std::vector<MyPixel> texture_data_;
+
+    unsigned int texture_width_;
+    unsigned int texture_heigth_;
+};
+
+
+class MyTexture {
+public:
+    MyTexture(const std::string&, double, double);
+    MyTexture(MyTexture*, double, double);
+    MyTexture() = delete;
+    ~MyTexture();
+
+    TextureSharedState* get_shared_state() const;
+    MyPixel pick_pickel(double, double) const;
+
+private:
+    double reflection_coeff_;
+    double scale_coeff_;
+
+    bool is_owner_;
+    TextureSharedState* shared_state_;
+};
+
+
+class TextureFactory {
+public:
+    TextureFactory() = default;
+    ~TextureFactory() = default;
+
+    MyTexture* get_texture(const std::string&, double, double);
+
+private:
+    std::list<MyTexture> textures_;
+    std::unordered_map<std::string, MyTexture*> texture_map_;
+};
+
 
 class Texture {
 public:
