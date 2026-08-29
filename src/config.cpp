27e02201ae9fdf6ca68787140fb725c68b4b9f34
efliks <mfeliks83@gmail.com>
2026-08-29 14:24:15 +0200

    Remove TOML parser, keep only text world-file format

--- src/config.cpp
+++ src/config.cpp
@@ -1,131 +1,17 @@
 #include <fstream>
 #include <sstream>
-#include <limits>  // req. by cpptoml with DJGPP
 #include <map>
 #include <cstdlib>
 
-#include "cpptoml.h"
 #include "config.h"
 #include "logger.h"
 
 
 namespace mrtp {
 
-class TomlTable : public ConfigTable
-{
-public:
-    TomlTable(std::shared_ptr<cpptoml::table> table)
-        : t_(table)
-    {
-    }
-
-    ~TomlTable() override = default;
-
-    double get_value(const std::string& key, double val_default) override
-    {
-        return t_->get_as<double>(key).value_or(val_default);
-    }
-
-    Vector3d get_vector(const std::string& key, const Vector3d& vec_default) override
-    {
-        auto x = t_->get_array_of<double>(key);
-        if (!x) {
-            return vec_default;
-        }
-
-        return Vector3d{x->data()};
-    }
-
-    Vector3d get_vector(const std::string& key) override
-    {
-        auto x = t_->get_array_of<double>(key);
-        if (!x) {
-            return Vector3d{};
-        }
-
-        return Vector3d{x->data()};
-    }
-
-    std::string get_text(const std::string& key) override
-    {
-        auto x = t_->get_as<std::string>(key);
-        if (!x) {
-            return std::string{};
-        }
-
-        return std::string{x->data()};
-    }
-
-private:
-    std::shared_ptr<cpptoml::table> t_;
-};
-
-
-class TomlTableIterator : public ConfigTableIterator
-{
-public:
-    TomlTableIterator(std::shared_ptr<cpptoml::table_array> ta)
-        : ta_(ta)
-    {
-    }
-
-    ~TomlTableIterator() override = default;
-
-    void first() override
-    {
-       iter_ = ta_->begin();
-    }
-
-    void next() override
-    {
-        ++iter_;
-    }
-
-    bool is_done() override
-    {
-        return iter_ == ta_->end();
-    }
-
-    std::shared_ptr<ConfigTable> current() override
-    {
-        return std::shared_ptr<ConfigTable>(new TomlTable(*iter_));
-    }
-
-private:
-    std::shared_ptr<cpptoml::table_array> ta_;
-    cpptoml::table_array::iterator iter_;
-};
-
-
-class TomlReader : public ConfigReader
-{
-public:
-    TomlReader(std::shared_ptr<cpptoml::table> toml_config)
-        : c_(toml_config)
-    {
-    }
-
-    ~TomlReader() override = default;
-
-    std::shared_ptr<ConfigTableIterator> get_tables(const std::string& array_name) override
-    {
-        std::shared_ptr<cpptoml::table_array> table_array = c_->get_table_array(array_name);
-        if (!table_array) {
-            return std::shared_ptr<ConfigTableIterator>();
-        }
-
-        return std::shared_ptr<ConfigTableIterator>(new TomlTableIterator(table_array));
-    }
-
-private:
-    std::shared_ptr<cpptoml::table> c_;
-};
-
-
 // ---------------------------------------------------------------- Text format
 //
-// A simple, line-oriented alternative to TOML. Each non-empty line describes
-// one row and has the form:
+// Each non-empty line describes one row and has the form:
 //
 //     type(key = value; key = value, value, value; ...)
 //
@@ -305,23 +191,13 @@ private:
 };
 
 
-std::shared_ptr<ConfigReader> open_toml_config(const std::string& filename)
-{
-    std::shared_ptr<cpptoml::table> config;
-    try {
-        config = cpptoml::parse_file(filename);
-    } catch (...) {
-        LOG_ERROR("Error parsing world file");
-        return std::shared_ptr<ConfigReader>();
-    }
-
-    return std::shared_ptr<ConfigReader>(new TomlReader(config));
-}
-
-
-std::shared_ptr<ConfigReader> open_text_config(const std::string& filename)
+std::shared_ptr<ConfigReader> open_config(const std::string& filename)
 {
     std::ifstream in(filename);
+    if (!in.good()) {
+        LOG_ERROR("Cannot open world file");
+        return std::shared_ptr<ConfigReader>();
+    }
 
     std::vector<std::pair<std::string, std::shared_ptr<ConfigTable>>> rows;
 
@@ -348,21 +224,6 @@ std::shared_ptr<ConfigReader> open_text_config(const std::string& filename)
     }
 
     return std::shared_ptr<ConfigReader>(new TextReader(rows));
-}
-
-
-std::shared_ptr<ConfigReader> open_config(const std::string& filename)
-{
-    std::fstream check(filename);
-    if (!check.good()) {
-        LOG_ERROR("Cannot open world file");
-        return std::shared_ptr<ConfigReader>();
-    }
-
-    bool is_toml = filename.size() >= 5 &&
-            filename.compare(filename.size() - 5, 5, ".toml") == 0;
-
-    return is_toml ? open_toml_config(filename) : open_text_config(filename);
 }
 
 
